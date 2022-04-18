3516d6ce3629fa9be3d97eabadf9ad46683c49dd
Folka2134 <folka2134@gmail.com>
2022-04-18 15:40:26 +0100

    Created navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { Home } from './components/Home.component';
+import { Navbar } from './components/Navbar.component';
 
 function App() {
   return (
     <div className="h-screen bg-purple-900">
+      <Navbar />
       <Home />
     </div>
   );
---
+++ src/components/Navbar.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div className='bg-green-200 h-12 flex justify-evenly items-center'>
+      <ul className='flex'>
+        <li className='px-4 cursor-pointer'>Home</li>
+        <li className='px-4 cursor-pointer'>About Us</li>
+        <li className='px-4 cursor-pointer'>Packages</li>
+        <li className='px-4 cursor-pointer'>Contact</li>
+      </ul>
+      <ul className='flex ml-52'>
+        <li className='px-4 cursor-pointer'>Account</li>
+        <li className='px-4 cursor-pointer'>Logout</li>
+      </ul>
+    </div>
+  )
+}
