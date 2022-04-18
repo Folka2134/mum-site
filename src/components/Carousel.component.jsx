e560ae439eeec47f83e462fb4a09f0d898f6b724
Folka2134 <folka2134@gmail.com>
2022-04-18 15:32:57 +0100

    Updated carousel spacing

--- src/components/Carousel.component.jsx
+++ src/components/Carousel.component.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Carousel = () => {
   return (
-    <div className='bg-red-400 h-[400px] w-3/4'>Carousel</div>
+    <div className='bg-red-400 h-[400px] w-3/4 mt-32'>Carousel</div>
   )
 }
--- src/components/Home.component.jsx
+++ src/components/Home.component.jsx
@@ -4,7 +4,7 @@ import { FeaturedProducts } from './FeaturedProducts.component'
 
 export const Home = () => {
   return (
-    <div className='h-screen bg-purple-400 mx-48 flex flex-col items-center'>
+    <div className='h-screen bg-purple-400 mx-32 flex flex-col items-center'>
       <Carousel />
       <FeaturedProducts />
     </div>
