fbbe780702445b27e3d7d9a80289a2d2511ecfe2
Folka2134 <folka2134@gmail.com>
2022-04-18 15:26:38 +0100

    Created Carousel component

---
+++ src/components/Carousel.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Carousel = () => {
+  return (
+    <div className='bg-red-400 h-[400px] w-[700px]'>Carousel</div>
+  )
+}
--- src/components/Home.component.jsx
+++ src/components/Home.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Carousel } from './Carousel.component'
 
 export const Home = () => {
   return (
-    <div className='bg-purple-400'>
-      Home
+    <div className='h-screen bg-purple-400 mx-48 flex flex-col items-center'>
+      <Carousel />
     </div>
   )
 }
