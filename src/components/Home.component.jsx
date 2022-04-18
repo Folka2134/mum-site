707b0faa34d0688a1b758de9389f99dedcdbeff0
Folka2134 <folka2134@gmail.com>
2022-04-18 15:57:58 +0100

    Created maincontent component

--- src/components/Home.component.jsx
+++ src/components/Home.component.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import { Carousel } from './Carousel.component'
 import { Cover } from './Cover.component'
-import { FeaturedProducts } from './FeaturedProducts.component'
+import { MainContent } from './MainContent.component'
 
 export const Home = () => {
   return (
-    <div className='h-screen bg-purple-400 mx-32 flex flex-col items-center'>
-      {/* <Carousel /> */}
+    <div className='h-full bg-purple-400 mx-32 flex flex-col items-center'>
       <Cover />
-      {/* <FeaturedProducts /> */}
+      <MainContent />
     </div>
   )
 }
---
+++ src/components/MainContent.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const MainContent = () => {
+  return (
+    <div className='bg-blue-300 h-screen w-full mt-12 pt-24'>MainContent</div>
+  )
+}
