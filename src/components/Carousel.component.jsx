7906a78d118d85e8fc497cd8a4169afc5dcbdba0
Folka2134 <folka2134@gmail.com>
2022-04-18 15:30:23 +0100

    Created Featured Products component

--- src/components/Carousel.component.jsx
+++ src/components/Carousel.component.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Carousel = () => {
   return (
-    <div className='bg-red-400 h-[400px] w-[700px]'>Carousel</div>
+    <div className='bg-red-400 h-[400px] w-3/4'>Carousel</div>
   )
 }
---
+++ src/components/FeaturedProducts.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const FeaturedProducts = () => {
+  return (
+    <div className='bg-cyan-400 w-full mt-32 h-1/2'>
+      Featured Products
+    </div>
+  )
+}
