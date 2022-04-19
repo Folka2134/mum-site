15a67f8dd0c82be83d88def10f72b89f0c4cb9d9
Folka2134 <folka2134@gmail.com>
2022-04-19 19:21:45 +0100

    Updated navbar burger functionality

--- src/components/Navbar.component.jsx
+++ src/components/Navbar.component.jsx
@@ -3,6 +3,14 @@ import React, { useState } from 'react'
 export const Navbar = () => {
   const [openNav, setOpenNav] = useState(false)
 
+  const updateMenu = () => {
+    if (openNav === false) {
+      setOpenNav(true)
+    } else {
+      setOpenNav(false)
+    }
+  }
+
   return (
     <div className='bg-green-200 h-12 flex justify-center items-center sticky top-0'>
       <div className='bg-green-600 h-12 w-full hidden md:flex justify-between items-center mx-32'>
@@ -17,23 +25,23 @@ export const Navbar = () => {
           <li className='px-4 cursor-pointer'>Logout</li>
         </ul>
       </div>
-      <div className='bg-pink-700 h-12 w-full flex md:hidden justify-end  mx-12'>
+      <div className='bg-pink-700 h-12 w-full flex md:hidden justify-end mx-12'>
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          class="w-6 h-6 mr-4 cursor-pointer text-purple-200 mt-3"
+          className={`w-6 h-6 mr-4 cursor-pointer text-purple-200 mt-3 ${openNav ? "rotate-90" : ""} transition-all`}
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
-          onClick={() => setOpenNav(true)}
+          onClick={() => updateMenu()}
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="M4 6h16M4 12h16M4 18h16"
           />
         </svg>
-        <div className='w-52 mt-12 hidden'>
+        <div className={`w-52 mt-12 absolute ${openNav ? "flex flex-col" : "hidden"}`}>
           <ul className='bg-blue-800 text-white flex flex-col items-center rounded-t-sm'>
             <li className='p-2 cursor-pointer'>Home</li>
             <li className='p-2 cursor-pointer whitespace-nowrap'>About Us</li>
