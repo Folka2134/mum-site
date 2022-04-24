c33afa30a4b4dcad008d740d697e88b3a8bb835a
Folka2134 <folka2134@gmail.com>
2022-04-24 13:26:27 +0100

    Updated styles

--- src/components/homePage/MainContent.component.jsx
+++ src/components/homePage/MainContent.component.jsx
@@ -3,7 +3,7 @@ import { PackagePre } from './PackagePre.component'
 
 export const MainContent = () => {
   return (
-    <div className='bg-blue-300 h-full min-h-screen w-full mt-12 pt-24 grid place-items-center'>
+    <div className='h-full min-h-screen w-full mt-12 pt-24 grid place-items-center border-t-2'>
       <PackagePre />
     </div>
   )
--- src/components/homePage/PackagePre.component.jsx
+++ src/components/homePage/PackagePre.component.jsx
@@ -2,49 +2,52 @@ import React from 'react'
 
 export const PackagePre = () => {
   return (
-    <div className='grid grid-cols-1 lg:grid-cols-2 gap-12 md:m-6' >
-      <div className=' border-2 border-black bg-purple-200'>
-        <div className='h-72 bg-package-bg w-full bg-center'></div>
-        <div className='flex flex-col items-center justify-center p-20 pt-12'>
-          <h1 className='text-2xl text-center mb-5'>Package Title</h1>
-          <p className='text-center'>Short description...
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo quis corrupti assumenda aspernatur,
-            rerum quo quos laborum at esse eveniet neque praesentium voluptate minus eius? </p>
-          <button className='mt-8 mb-1 p-4 bg-pink-500 w-52'>Overview</button>
-          <button className='p-4 bg-pink-500 w-52'>Unlock</button>
+    <div className='text-center'>
+      <h1 className='text-[36px] mb-12 font-semibold'>Our Packages</h1>
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-12 md:m-6'>
+        <div className='border-2 border-black rounded'>
+          <div className='h-72 bg-package-bg w-full bg-center'></div>
+          <div className='flex flex-col items-center justify-center p-20 pt-12'>
+            <h1 className='text-2xl text-center mb-5'>Package Title</h1>
+            <p className='text-center'>Short description...
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo quis corrupti assumenda aspernatur,
+              rerum quo quos laborum at esse eveniet neque praesentium voluptate minus eius? </p>
+            <button className='mt-8 mb-1 p-4 bg-pink-500 w-52'>Overview</button>
+            <button className='p-4 bg-pink-500 w-52'>Unlock</button>
+          </div>
         </div>
-      </div>
-      <div className='border-2 border-black bg-purple-200'>
-        <div className='h-72 bg-package2-bg w-full bg-center'></div>
-        <div className='flex flex-col items-center p-20'>
-          <h1 className='text-2xl text-center mb-5'>Package Title</h1>
-          <p className='text-center'>Short description...
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo quis corrupti assumenda aspernatur,
-            rerum quo quos laborum at esse eveniet neque praesentium voluptate minus eius? </p>
-          <button className='mt-8 mb-1  p-4 bg-pink-500 w-52'>Overview</button>
-          <button className='p-4 bg-pink-500 w-52'>Unlock</button>
+        <div className='border-2 border-black rounded'>
+          <div className='h-72 bg-package2-bg w-full bg-center'></div>
+          <div className='flex flex-col items-center p-20'>
+            <h1 className='text-2xl text-center mb-5'>Package Title</h1>
+            <p className='text-center'>Short description...
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo quis corrupti assumenda aspernatur,
+              rerum quo quos laborum at esse eveniet neque praesentium voluptate minus eius? </p>
+            <button className='mt-8 mb-1  p-4 bg-pink-500 w-52'>Overview</button>
+            <button className='p-4 bg-pink-500 w-52'>Unlock</button>
+          </div>
         </div>
-      </div>
-      <div className='border-2 border-black bg-purple-200'>
-        <div className='h-72 bg-package4-bg w-full bg-center'></div>
-        <div className='flex flex-col items-center p-20'>
-          <h1 className='text-2xl text-center mb-5'>Package Title</h1>
-          <p className='text-center'>Short description...
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo quis corrupti assumenda aspernatur,
-            rerum quo quos laborum at esse eveniet neque praesentium voluptate minus eius? </p>
-          <button className='mt-8 mb-1  p-4 bg-pink-500 w-52'>Overview</button>
-          <button className='p-4 bg-pink-500 w-52'>Unlock</button>
+        <div className='border-2 border-black rounded'>
+          <div className='h-72 bg-package4-bg w-full bg-center'></div>
+          <div className='flex flex-col items-center p-20'>
+            <h1 className='text-2xl text-center mb-5'>Package Title</h1>
+            <p className='text-center'>Short description...
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo quis corrupti assumenda aspernatur,
+              rerum quo quos laborum at esse eveniet neque praesentium voluptate minus eius? </p>
+            <button className='mt-8 mb-1  p-4 bg-pink-500 w-52'>Overview</button>
+            <button className='p-4 bg-pink-500 w-52'>Unlock</button>
+          </div>
         </div>
-      </div>
-      <div className='border-2 border-black bg-purple-200'>
-        <div className='h-72 bg-package3-bg w-full bg-center'></div>
-        <div className='flex flex-col items-center p-20'>
-          <h1 className='text-2xl text-center mb-5'>Package Title</h1>
-          <p className='text-center'>Short description...
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo quis corrupti assumenda aspernatur,
-            rerum quo quos laborum at esse eveniet neque praesentium voluptate minus eius? </p>
-          <button className='mt-8 mb-1  p-4 bg-pink-500 w-52'>Overview</button>
-          <button className='p-4 bg-pink-500 w-52'>Unlock</button>
+        <div className='border-2 border-black rounded'>
+          <div className='h-72 bg-package3-bg w-full bg-center'></div>
+          <div className='flex flex-col items-center p-20'>
+            <h1 className='text-2xl text-center mb-5'>Package Title</h1>
+            <p className='text-center'>Short description...
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo quis corrupti assumenda aspernatur,
+              rerum quo quos laborum at esse eveniet neque praesentium voluptate minus eius? </p>
+            <button className='mt-8 mb-1  p-4 bg-pink-500 w-52'>Overview</button>
+            <button className='p-4 bg-pink-500 w-52'>Unlock</button>
+          </div>
         </div>
       </div>
     </div>
