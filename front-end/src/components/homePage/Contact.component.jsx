f511214bed1f59226d617f2587b5213474ba283f
Folka2134 <folka2134@gmail.com>
2022-04-25 21:33:58 +0100

    Completed contact functionality

--- front-end/src/components/homePage/Contact.component.jsx
+++ front-end/src/components/homePage/Contact.component.jsx
@@ -1,28 +1,38 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import emailjs from 'emailjs-com'
 
 export const Contact = () => {
+  const form = useRef()
 
   const submit = (e) => {
     e.preventDefault()
+
+    emailjs.sendForm('service_4aukl6p', 'template_3e5af5d', form.current, 'vGFjAO9uY2LbF2Q5A')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+    form.current.reset()
   }
 
   return (
     <div className='contact my-24 py-24 border-t-2 border-b-2 text-center'>
       <h1 className='text-3xl font-bold'>Contact us</h1>
       <p className='text-lg mt-5'>Get in touch for any queries regarding our packages or to book a free consultation!</p>
-      <form onSubmit={submit} className="w-full mt-12 grid justify-center">
+      <form onSubmit={submit} ref={form} className="w-full mt-12 grid justify-center">
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
               First Name
             </label>
-            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" required />
+            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" required name="from_fname" />
           </div>
           <div className="w-full md:w-1/2 px-3">
             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-last-name">
               Last Name
             </label>
-            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" required />
+            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" required name="from_lname" />
           </div>
         </div>
         <div className="flex flex-wrap -mx-3 mb-6">
@@ -30,7 +40,7 @@ export const Contact = () => {
             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
               E-mail
             </label>
-            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" required />
+            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" required name="email" />
           </div>
         </div>
         <div className="flex flex-wrap -mx-3 mb-6">
@@ -38,7 +48,7 @@ export const Contact = () => {
             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
               Message
             </label>
-            <textarea className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message" required></textarea>
+            <textarea className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message" required name="message"></textarea>
           </div>
         </div>
         <div className="md:flex md:items-center">
