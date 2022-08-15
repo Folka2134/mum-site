d988e34720817bf3faa9688a528e3689252d3f15
Folka2134 <folka2134@gmail.com>
2022-08-15 10:29:06 +0100

    updated footer colour

--- src/components/Footer.component.jsx
+++ src/components/Footer.component.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div className='bg-green-200 h-12 w-full'></div>
+    <div className='bg-[#D9E7E8] h-12 w-full'></div>
   )
 }
