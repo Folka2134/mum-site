293c657b4483e99795bb33f68e685424d9c5716b
Folka2134 <folka2134@gmail.com>
2022-11-28 11:14:59 +0000

    created packagePage

---
+++ src/pages/package/PackagePage.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const PackagePage = () => {
+  return (
+    <div className="h-screen top-0">
+      <title>Package</title>
+      <main className="flex">
+        <div className="bg-purple-500">
+          <img src="#" alt="#" />
+          <ul></ul>
+        </div>
+        <div className="bg-purple-700">
+          <p></p>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default PackagePage;
