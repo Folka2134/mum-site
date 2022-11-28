bd44c8853b87b08132c82f6de9c5840e7322a586
Folka2134 <folka2134@gmail.com>
2022-11-28 10:44:54 +0000

    created packageData to import into prepackage and packagePage

---
+++ src/data/data.js
@@ -0,0 +1,21 @@
+const packageData = [
+  {
+    id: "package1",
+    title: "package1",
+    mini_desc:
+      "Short description about package1... Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo quis corrupti assumenda aspernatur, rerum quo quos laborum at esse eveniet neque praesentium voluptate minuseius?",
+  },
+  {
+    id: "package2",
+    title: "package2",
+    mini_desc:
+      "Short description about package2... Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo quis corrupti assumenda aspernatur, rerum quo quos laborum at esse eveniet neque praesentium voluptate minuseius?",
+  },
+  // {
+  //   id: "package3",
+  //   title: "package3",
+  //   main: "Short description about package2... Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo quis corrupti assumenda aspernatur, rerum quo quos laborum at esse eveniet neque praesentium voluptate minuseius?",
+  // },
+];
+
+export default packageData;
