79162a8bf1a982a58d6463c1bf48530504aed9f1
Folka2134 <folka2134@gmail.com>
2022-11-28 10:44:25 +0000

    renamed image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,16 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      backgroundImage: { 
-        'logo-bg' : "url('./images/Baobab-Adansonia.jpg')",
-        'package-bg' : "url('./images/package.jpg')",
-        'package2-bg' : "url('./images/package2.jpg')",
-        'package3-bg' : "url('./images/package3.jpg')",
-        'package4-bg' : "url('./images/package4.jpg')",
-        'about-pic' : "url('./images/innovation-coaching.jpg')",
-      }
+      backgroundImage: {
+        "logo-bg": "url('./images/Baobab-Adansonia.jpg')",
+        "package1-bg": "url('./images/package1.jpg')",
+        "package2-bg": "url('./images/package2.jpg')",
+        "package3-bg": "url('./images/package3.jpg')",
+        "package4-bg": "url('./images/package4.jpg')",
+        "about-pic": "url('./images/innovation-coaching.jpg')",
+      },
     },
   },
   plugins: [],
-}
+};
