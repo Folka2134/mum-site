aeb232482895ea8c8ebaea7262223d0b63e26bbb
Folka2134 <folka2134@gmail.com>
2022-12-05 10:52:13 +0000

    updated font size

--- src/pages/homePage/PackagePre.component.jsx
+++ src/pages/homePage/PackagePre.component.jsx
@@ -5,7 +5,7 @@ export const PackagePre = ({ packages }) => {
   console.log(packages);
   return (
     <div className=" packages text-center border-t-2 pt-44">
-      <h1 className="text-[36px] mb-12 font-semibold">Our Packages</h1>
+      <h1 className="text-[38px] mb-12 font-semibold">Our Packages</h1>
       <div className="package-container grid grid-cols-1 lg:grid-cols-2 gap-12 md:m-6">
         {packages.map((packageInfo) => (
           <div key={packageInfo.id} className="border-black rounded">
