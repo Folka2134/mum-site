409bbb00dc55b134f6070c9486eac07ed77c73ed
Folka2134 <folka2134@gmail.com>
2022-12-06 10:28:11 +0000

    reverted to lesser version of react-router syntax

--- src/index.js
+++ src/index.js
@@ -1,30 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
-
-import ErrorPage from "./pages/error-page";
-import PackagePage from "./pages/package/PackagePage.component";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 
-import packageData from "./data/data";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "packagePage/:id",
-    element: <PackagePage packageData={packageData} />,
-  },
-]);
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
