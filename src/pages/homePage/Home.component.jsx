ed6d456c868143b52f9c81c0a6782d0f82f0f44a
Folka2134 <folka2134@gmail.com>
2022-12-06 12:44:30 +0000

    added navbar and footer

--- src/pages/homePage/Home.component.jsx
+++ src/pages/homePage/Home.component.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import { Cover } from './Cover.component'
-import { MainContent } from './MainContent.component'
+import React from "react";
+import { Cover } from "./Cover.component";
+import { MainContent } from "./MainContent.component";
+import { Navbar } from "../../components/Navbar.component";
+import { Footer } from "../../components/Footer.component";
 
 export const Home = () => {
   return (
-    <div className='h-full mx-6 lg:mx-12 xl:mx-32 flex flex-col items-center'>
+    <div className="h-full w-full  flex flex-col items-center">
+      <Navbar />
       <Cover />
       <MainContent />
+      <Footer />
     </div>
-  )
-}
+  );
+};
