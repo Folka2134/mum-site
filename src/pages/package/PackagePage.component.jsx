c3839ae5351ece342bf791f4d69b8adc1f2f6aaa
Folka2134 <folka2134@gmail.com>
2022-12-06 12:44:54 +0000

    built up package

--- src/pages/package/PackagePage.component.jsx
+++ src/pages/package/PackagePage.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 import packageData from "../../data/data";
 
@@ -11,13 +11,28 @@ const PackagePage = () => {
   }
 
   return (
-    <div className=" top-0 bg-gray-400">
-      <main className="flex">
-        <div className="bg-purple-500">
-          <h1>{packageInfo.title}</h1>
-          <img src="#" alt="#" />
+    <div className="top-0">
+      <nav className="bg-[#A8DADC] h-16 flex justify-center items-center sticky top-0 text-lg shadow-md ">
+        <Link to="/">
+          <button className="h-full px-4 cursor-pointer  hover:bg-[#F1FAEE] transition-all delay-75 duration-300 rounded-full">
+            Home
+          </button>
+        </Link>
+      </nav>
+      <main className="h-max flex flex-col justify-center">
+        <h1 className="text-3xl md:text-4xl self-center m-5">
+          {packageInfo.title}
+        </h1>
+        <div className="h-96 grid grid-cols-1 md:grid-cols-2 bg-purple-500">
+          <h1>video</h1>
+          <div className="flex flex-col justify-center items-center p-8 bg-green-200">
+            <p>{packageInfo.mini_desc}</p>
+            <button className="mt-10 p-4 text-white bg-[#457B9D] w-52 rounded border-gray-200  hover:bg-[#1D3557] duration-300">
+              Unlock
+            </button>
+          </div>
         </div>
-        <div className="bg-purple-700">
+        <div className="bg-purple-700 px-12 py-8 text-">
           <p>{packageInfo.disc}</p>
         </div>
       </main>
