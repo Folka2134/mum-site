82f37319dd13bf59e2e5242546b3e84de0e39845
Folka2134 <folka2134@gmail.com>
2022-12-06 10:27:47 +0000

    rendering a PackageMini per item within packData

--- src/pages/homePage/PackagePre.component.jsx
+++ src/pages/homePage/PackagePre.component.jsx
@@ -1,30 +1,14 @@
 import React from "react";
-import { Outlet, Link } from "react-router-dom";
+
+import { PackageMini } from "../../components/PackageMini.component";
 
 export const PackagePre = ({ packages }) => {
-  console.log(packages);
   return (
     <div className=" packages text-center border-t-2 pt-44">
       <h1 className="text-[38px] mb-12 font-semibold">Our Packages</h1>
       <div className="package-container grid grid-cols-1 lg:grid-cols-2 gap-12 md:m-6">
         {packages.map((packageInfo) => (
-          <div key={packageInfo.id} className="border-black rounded">
-            <div className={`h-72  w-full bg-center rounded-full shadow-lg`}>
-              <img src={packageInfo.img} alt="" />
-            </div>
-            <div className="flex flex-col items-center justify-center p-12">
-              <h1 className="text-2xl text-center mb-5">{packageInfo.title}</h1>
-              <p className="text-center">{packageInfo.mini_desc}</p>
-              <Link to={`packagePage/${packageInfo.id}`}>
-                <button className="mt-8 mb-1 p-4 text-white bg-[#457B9D] w-52 rounded  hover:bg-[#1D3557] duration-300">
-                  Overview
-                </button>
-              </Link>
-              <button className="p-4 text-white bg-[#457B9D] w-52 rounded border-gray-200  hover:bg-[#1D3557] duration-300">
-                Unlock
-              </button>
-            </div>
-          </div>
+          <PackageMini key={packageInfo.id} packageInfo={packageInfo} />
         ))}
       </div>
     </div>
