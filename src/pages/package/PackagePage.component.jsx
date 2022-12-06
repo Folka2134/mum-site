dc99f7fde156986bb09caf273ff96be13b97e08e
Folka2134 <folka2134@gmail.com>
2022-12-06 10:29:12 +0000

    rendering package info based on url parameter

--- src/pages/package/PackagePage.component.jsx
+++ src/pages/package/PackagePage.component.jsx
@@ -1,16 +1,24 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+
+import packageData from "../../data/data";
 
 const PackagePage = () => {
+  const { id } = useParams();
+
+  if (Object.values(packageData.includes(id))) {
+    var packageInfo = packageData[id];
+  }
+
   return (
-    <div className="h-screen top-0">
-      <title>Package</title>
+    <div className=" top-0 bg-gray-400">
       <main className="flex">
         <div className="bg-purple-500">
+          <h1>{packageInfo.title}</h1>
           <img src="#" alt="#" />
-          <ul></ul>
         </div>
         <div className="bg-purple-700">
-          <p></p>
+          <p>{packageInfo.disc}</p>
         </div>
       </main>
     </div>
