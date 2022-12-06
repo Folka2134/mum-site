0d8d9a238dae8431e5c0e8294363c1bfb7f3c99c
Folka2134 <folka2134@gmail.com>
2022-12-06 10:26:44 +0000

    implemented react-router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import "./App.css";
 import { Footer } from "./components/Footer.component";
 import { Home } from "./pages/homePage/Home.component";
 import { Navbar } from "./components/Navbar.component";
+import { Route, Routes } from "react-router-dom";
+import PackagePage from "./pages/package/PackagePage.component";
 
 function App() {
   return (
     <div className="home h-full min-w-[350px] bg-[#FAFAF8]">
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/packagePage/:id" element={<PackagePage />} />
+      </Routes>
       <Footer />
     </div>
   );
